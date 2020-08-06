3f61c0f09cdcda4618760902b791ecea738efd76
KeyMirror <tomas.ezequiel.rln@gmail.com>
2020-08-06 08:56:18 -0300

    Consultas dynamo

--- consultas.py
+++ consultas.py
@@ -7,14 +7,74 @@ dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
 matchesTable = dynamodb.Table('WorldMatches')
 playersTable = dynamodb.Table('WorldPlayers')
 cupsTable = dynamodb.Table('WorldCup')
-
-responseMatches = matchesTable.scan()
-responsePlayers = playersTable.scan()
-responseCups = cupsTable.scan()
+finalPlayersTable = dynamodb.Table('WorldPlayersFinal')
 
 
-responseMatches = matchesTable.scan(
-    FilterExpression=Attr('Year').eq(1930)
-)
-items = responseMatches['Items']
-print(items)
+def datos_segun_edicion(unAnio): 
+	responseCups = cupsTable.get_item(Key ={'Year':unAnio})
+
+	return responseCups['Item']
+
+
+
+
+def jugadores_argentina_final(): 
+	responseMatch = matchesTable.scan(
+    	FilterExpression=Attr('Stage').eq("Final") & (Attr('Home Team Name').eq("Argentina") | Attr('Away Team Name').eq("Argentina"))
+	)
+
+	datos = responseMatch['Items']
+	responsePlayers = finalPlayersTable.scan(
+	    FilterExpression = Attr('Team Initials').eq("ARG")
+	)
+
+	datosPlayers = responsePlayers['Items']
+
+	for i in datosPlayers: 
+	    print(i['Player Name'])
+
+
+def estadio_final(unAnio): 
+
+	responseMatch = matchesTable.scan(
+		FilterExpression = Attr('Stage').eq('Final') & Attr('Year').eq(unAnio)
+	)
+
+	datos = responseMatch['Items']
+
+	return datos[0]['Stadium']
+
+
+
+def estadios_maradona():
+	responsePlayers = playersTable.scan(
+		FilterExpression = Attr('Player Name').eq('Diego MARADONA')
+	)
+
+	datos = responsePlayers['Items']
+
+	matchIDs = list()
+
+	for i in datos: 
+		matchIDs.append(i['MatchID'])
+
+
+
+	datos_maradona = list()
+
+	for i in matchIDs: 
+		responseMatch = matchesTable.scan(
+			FilterExpression = Attr('MatchID').eq(i)
+		)
+
+		d = responseMatch['Items']
+
+		datos_maradona = datos_maradona + d 
+
+
+	for i in datos_maradona: 
+		print(i['Stadium'])
+
+
+
+estadios_maradona()
